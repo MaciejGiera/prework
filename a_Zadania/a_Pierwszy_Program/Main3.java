2b2e98f906d47b9fd2b39e3ba8709f035226d722
Maciek Giera <maciek.giera@gmail.com>
2019-10-04 13:04:22 +0200

    Rozwiązania zadania pierwszego

---
+++ a_Zadania/a_Pierwszy_Program/Main3.java
@@ -0,0 +1,17 @@
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		System.out.println("Pierwszy program JAVA");
+		//Komentarz w jednej linii
+
+		/*
+		Między tymi znacznikai wszystkie linnie są komentarzem
+
+
+		*/
+
+		/** Komentarz dokumentacyjny */
+	}
+
+}
---
+++ a_Zadania/a_Pierwszy_Program/Main5.java
@@ -0,0 +1,11 @@
+//class powinno nazywać się tak jak plik
+public class Main5 {
+
+	public static void main(String[] args) {
+		System.out.print("Dzisiaj zaczynam kurs");
+		System.out.print("języka JAVA");
+		System.out.print("w CodersLab");
+
+	}
+
+}
